957513441c92f8fee9ad9225371a887d6167a01e
MatchaTi <tehmatcha28@gmail.com>
2025-02-14 19:51:57 +0800

    feat: can select theme

--- src/components/CVForm.tsx
+++ src/components/CVForm.tsx
@@ -1,83 +1,50 @@
-import { useCVStore } from '../store/useCVStore';
+import clsx from 'clsx';
+import { useThemeStore } from '../store/useThemeStore';
+import { Theme } from '../type/theme';
 import { exportPDF } from '../util/PDF';
+import Brutalist from './form/Brutalist';
+import Elegant from './form/Elegant';
+import Formal from './form/Formal';
+import Playful from './form/Playful';
 
 export default function CVForm() {
-  const { data, setData, addEducation, updateEducation, removeEducation } = useCVStore();
+  const { theme, setTheme } = useThemeStore();
+
+  const themes: { name: Theme; component: React.FC }[] = [
+    { name: 'formal', component: Formal },
+    { name: 'playful', component: Playful },
+    { name: 'elegant', component: Elegant },
+    { name: 'brutalist', component: Brutalist },
+  ];
+
+  const SelectedTemplate = themes.find((t) => t.name === theme)?.component || Formal;
 
   return (
-    <div>
-      <div id='resumeResult' className='mx-auto max-w-2xl p-4'>
-        <h2>Personal Info</h2>
-        {Object.keys(data.personalInfo).map((key) => (
-          <input
-            key={key}
-            type='text'
-            className='bg-gray-100 p-4'
-            value={data.personalInfo[key as keyof typeof data.personalInfo]}
-            onChange={(e) =>
-              setData({
-                personalInfo: { ...data.personalInfo, [key]: e.target.value },
-              })
-            }
-            placeholder={key}
-          />
+    <div
+      className={clsx('', {
+        'font-petit-formal-script bg-red-500': theme === 'formal',
+        'font-pacifico bg-yellow-500': theme === 'playful',
+        'bg-green-500': theme === 'elegant',
+        'bg-blue-500': theme === 'brutalist',
+      })}
+    >
+      <div>
+        {themes.map(({ name }) => (
+          <button key={name} onClick={() => setTheme(name as Theme)}>
+            {name}
+          </button>
         ))}
-
-        <h2>Bio</h2>
-        <textarea
-          className='field-sizing-content max-h-20 w-full p-4'
-          value={data.bio}
-          onChange={(e) =>
-            setData({
-              bio: e.target.value,
-            })
-          }
-        ></textarea>
-
-        <h2>Social Media</h2>
-        {Object.keys(data.socialLinks).map((key) => (
-          <input
-            key={key}
-            type='text'
-            className='bg-gray-100 p-4'
-            value={data.socialLinks[key as keyof typeof data.socialLinks]}
-            onChange={(e) =>
-              setData({
-                socialLinks: { ...data.socialLinks, [key]: e.target.value },
-              })
-            }
-            placeholder={key}
-          />
-        ))}
-
-        <h2>Education</h2>
-        {data.education.map((edu, index) => (
-          <div key={index} className='education-item'>
-            <input
-              type='text'
-              value={edu.institute}
-              className='bg-gray-100 p-4'
-              onChange={(e) => updateEducation(index, 'institute', e.target.value)}
-              placeholder='School'
-            />
-            <input
-              type='text'
-              value={edu.degree}
-              onChange={(e) => updateEducation(index, 'degree', e.target.value)}
-              placeholder='Degree'
-            />
-            <input
-              type='text'
-              value={edu.year}
-              onChange={(e) => updateEducation(index, 'year', e.target.value)}
-              className='bg-gray-100 p-4'
-              placeholder='Year'
-            />
-            <button onClick={() => removeEducation(index)}>❌ Remove</button>
-          </div>
-        ))}
-
-        <button onClick={addEducation}>➕ Add Education</button>
+      </div>
+      <div
+        id='resumeResult'
+        className={clsx('mx-auto max-w-2xl p-4', {
+          'font-petit-formal-script bg-red-500': theme === 'formal',
+          'font-pacifico bg-yellow-500': theme === 'playful',
+          'bg-green-500': theme === 'elegant',
+          'bg-blue-500': theme === 'brutalist',
+        })}
+      >
+        <SelectedTemplate />
       </div>
       <button type='button' onClick={exportPDF}>
         Download Cuy
---
+++ src/components/form/Formal.tsx
@@ -0,0 +1,81 @@
+import { useCVStore } from '../../store/useCVStore';
+
+export default function Formal() {
+  const { data, setData, addEducation, updateEducation, removeEducation } = useCVStore();
+
+  return (
+    <>
+      <h2>Formal</h2>
+      <h2>Personal Info</h2>
+      {Object.keys(data.personalInfo).map((key) => (
+        <input
+          key={key}
+          type='text'
+          className='bg-gray-100 p-4'
+          value={data.personalInfo[key as keyof typeof data.personalInfo]}
+          onChange={(e) =>
+            setData({
+              personalInfo: { ...data.personalInfo, [key]: e.target.value },
+            })
+          }
+          placeholder={key}
+        />
+      ))}
+
+      <h2>Bio</h2>
+      <textarea
+        className='field-sizing-content max-h-20 w-full p-4'
+        value={data.bio}
+        onChange={(e) =>
+          setData({
+            bio: e.target.value,
+          })
+        }
+      ></textarea>
+
+      <h2>Social Media</h2>
+      {Object.keys(data.socialLinks).map((key) => (
+        <input
+          key={key}
+          type='text'
+          className='bg-gray-100 p-4'
+          value={data.socialLinks[key as keyof typeof data.socialLinks]}
+          onChange={(e) =>
+            setData({
+              socialLinks: { ...data.socialLinks, [key]: e.target.value },
+            })
+          }
+          placeholder={key}
+        />
+      ))}
+
+      <h2>Education</h2>
+      {data.education.map((edu, index) => (
+        <div key={index} className='education-item'>
+          <input
+            type='text'
+            value={edu.institute}
+            className='bg-gray-100 p-4'
+            onChange={(e) => updateEducation(index, 'institute', e.target.value)}
+            placeholder='School'
+          />
+          <input
+            type='text'
+            value={edu.degree}
+            onChange={(e) => updateEducation(index, 'degree', e.target.value)}
+            placeholder='Degree'
+          />
+          <input
+            type='text'
+            value={edu.year}
+            onChange={(e) => updateEducation(index, 'year', e.target.value)}
+            className='bg-gray-100 p-4'
+            placeholder='Year'
+          />
+          <button onClick={() => removeEducation(index)}>❌ Remove</button>
+        </div>
+      ))}
+      <button onClick={addEducation}>➕ Add Education</button>
+    </>
+  );
+}
