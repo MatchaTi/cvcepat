10e7514879d3211c6f5fceafe5bb8d9ad176a224
MatchaTi <tehmatcha28@gmail.com>
2025-02-14 21:28:20 +0800

    feat: add placeholders for input

---
+++ src/constant/placeholders.ts
@@ -0,0 +1,17 @@
+export const personalPlaceholders: string[] = [
+  'Cth: Adi Muhammad Syifai',
+  'Cth: Frontend Developer',
+  'Cth: 081234567890',
+  'Cth: adi@gmail.com',
+  'Cth: Indonesia, Samarinda',
+];
+
+export const socialLinksPlaceholders: string[] = ['facebook', 'github', 'instagram', 'linkedin', 'twitter'];
+
+export const socialLinksURLPlaceholders: string[] = [
+  'https://facebook.com',
+  'https://github.com',
+  'https://instagram.com',
+  'https://linkedin.com',
+  'https://twitter.com',
+];
