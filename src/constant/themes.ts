a4333fefd8f6891aa4521dd0ef006abea00641a0
MatchaTi <tehmatcha28@gmail.com>
2025-02-16 20:16:05 +0800

    feat: add retro theme

--- src/constant/themes.ts
+++ src/constant/themes.ts
@@ -1,12 +1,13 @@
 import Brutalist from '../components/form/Brutalist';
-import Elegant from '../components/form/Elegant';
 import Formal from '../components/form/Formal';
 import Playful from '../components/form/Playful';
+import Retro from '../components/template/Retro';
 import { Theme } from '../type/theme';
 
 export const themes: { name: Theme; component: React.FC }[] = [
-  { name: 'formal', component: Formal },
-  { name: 'playful', component: Playful },
-  { name: 'elegant', component: Elegant },
-  { name: 'brutalist', component: Brutalist },
+  { name: 'light', component: Formal },
+  { name: 'dark', component: Playful },
+  { name: 'retro', component: Retro },
+  { name: 'luxury', component: Brutalist },
+  { name: 'pastel', component: Brutalist },
 ];
