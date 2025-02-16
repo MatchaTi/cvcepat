d0fb401feccda12e1d5486c6385b076aab8c6333
MatchaTi <tehmatcha28@gmail.com>
2025-02-16 20:13:48 +0800

    feat: add theme switcher

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import clsx from 'clsx';
+import { themes } from '../constant/themes';
+import { useThemeStore } from '../store/useThemeStore';
+
+export default function ThemeSwitcher() {
+  const { theme, setTheme } = useThemeStore();
+
+  return (
+    <div className='mb-10 flex items-center justify-center gap-3'>
+      {themes.map(({ name }) => (
+        <button
+          type='button'
+          key={name}
+          onClick={() => setTheme(name)}
+          className={clsx('flex cursor-pointer items-center gap-1 p-3 active:scale-95', {
+            'bg-retro-accent text-retro-content hover:bg-retro-darker rounded-xl': theme === 'retro',
+          })}
+        >
+          <Icon icon={theme == name ? 'charm:circle-tick' : 'material-symbols:circle-outline'} />
+          <span className='capitalize'>{name}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
