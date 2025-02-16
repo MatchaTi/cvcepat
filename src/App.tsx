a6655d15615f21bdde9d098e97510fbb8eb47d02
MatchaTi <tehmatcha28@gmail.com>
2025-02-16 20:18:48 +0800

    chore: nahh

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import clsx from 'clsx';
 import './App.css';
 import CVForm from './components/CVForm';
-import { useCVStore } from './store/useCVStore';
+import Navbar from './components/Navbar';
+import Light from './components/template/Light';
+import ThemeSwitcher from './components/ThemeSwitcher';
+import { themes } from './constant/themes';
+import { useThemeStore } from './store/useThemeStore';
+import { useWorkspaceStore } from './store/useWorkspaceStore';
 
-function App() {
-  const { data } = useCVStore();
-  console.log(data);
+export default function App() {
+  const { theme } = useThemeStore();
+  const { status } = useWorkspaceStore();
+
+  const SelectedTemplate = themes.find((t) => t.name === theme)?.component || Light;
 
   return (
-    <div className='h-dvh w-dvw bg-white font-sans text-black'>
-      <CVForm />
+    <div
+      className={clsx('min-h-fit w-full pb-10', {
+        'bg-retro-base text-retro-content font-abhaya-libre': theme === 'retro',
+      })}
+    >
+      <Navbar />
+      <ThemeSwitcher />
+      {status === 'editor' ? (
+        <CVForm />
+      ) : (
+        <div id='resumeResult'>
+          <SelectedTemplate />
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
