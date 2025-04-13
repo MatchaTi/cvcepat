4375f46000bc02da472ace5aeec095415cbbce61
MatchaTi <tehmatcha28@gmail.com>
2025-04-13 10:52:14 +0800

    feat: add main layout for page

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,20 @@
+import LayoutSwitcher from './LayoutSwitcher';
+import Navbar from './Navbar';
+import ThemeSwitcher from './ThemeSwitcher';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <div className='px-6 pb-10 w-full min-h-fit'>
+      <Navbar />
+      <div className='flex flex-wrap gap-3 mx-auto w-full md:justify-center md:items-center'>
+        <ThemeSwitcher />
+        <LayoutSwitcher />
+      </div>
+      {children}
+    </div>
+  );
+}
