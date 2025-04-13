ff4041d116c4a7f8dabdc6947552a55a1a1d76bd
MatchaTi <tehmatcha28@gmail.com>
2025-04-13 11:57:04 +0800

    chore: add panduan route

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Form from './pages/Form.tsx';
 import Result from './pages/Result.tsx';
+import NotFound from './components/NotFound.tsx';
+import Panduan from './pages/Panduan.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path='/' element={<App />} />
         <Route path='/form' element={<Form />} />
         <Route path='/result' element={<Result />} />
+        <Route path='/panduan' element={<Panduan />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
