eedac610cf3e1a8ff15530859f534ba9b4887528
MatchaTi <tehmatcha28@gmail.com>
2025-04-13 10:55:36 +0800

    chore: update constant themes

--- src/constant/themes.ts
+++ src/constant/themes.ts
@@ -1,18 +1,11 @@
-import Dark from '../components/template/Dark';
-import Elegant from '../components/template/Elegant';
-import Formal from '../components/template/Formal';
-import Light from '../components/template/Light';
-import Luxury from '../components/template/Luxury';
-import Pastel from '../components/template/Pastel';
-import Retro from '../components/template/Retro';
 import { Theme } from '../type/theme';
 
-export const themes: { name: Theme; component: React.FC }[] = [
-  { name: 'formal', component: Formal },
-  { name: 'light', component: Light },
-  { name: 'dark', component: Dark },
-  { name: 'retro', component: Retro },
-  { name: 'luxury', component: Luxury },
-  { name: 'pastel', component: Pastel },
-  { name: 'elegant', component: Elegant },
+export const themes: { name: Theme }[] = [
+  { name: 'cyberpunk' },
+  { name: 'light' },
+  { name: 'dark' },
+  { name: 'retro' },
+  { name: 'aqua' },
+  { name: 'synthwave' },
+  { name: 'forest' },
 ];
