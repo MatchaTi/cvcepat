19eb93d3f2d336ac40def2ad326200e6e1d5e9ed
MatchaTi <tehmatcha28@gmail.com>
2025-04-15 13:47:25 +0800

    fix: fixed bug z index

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className='px-6 pb-10 w-full font-inter min-h-fit'>
       <Navbar />
-      <div className='flex flex-wrap gap-3 mx-auto mb-10 w-full md:justify-center md:items-center'>
+      <div className='flex relative z-10 flex-wrap gap-3 mx-auto mb-10 w-full md:justify-center md:items-center'>
         <ThemeSwitcher />
         <LayoutSwitcher />
       </div>
